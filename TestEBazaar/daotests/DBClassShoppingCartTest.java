d9834a0593be09cb8f75584785a6a43d9f249308
mamadou <diarraoa@gmail.com>
2015-04-22 11:32:04 -0500

    DBClassShoppingCartTest ..

--- TestEBazaar/daotests/DBClassShoppingCartTest.java
+++ TestEBazaar/daotests/DBClassShoppingCartTest.java
@@ -8,12 +8,10 @@ import java.util.logging.Logger;
 import middleware.exceptions.DatabaseException;
 import dbsetup.DbQueries;
 import business.customersubsystem.CustomerSubsystemFacade;
-import business.exceptions.BackendException;
 import business.externalinterfaces.CustomerProfile;
 import business.externalinterfaces.CustomerSubsystem;
 import business.externalinterfaces.DbClassShoppingCartForTest;
 import business.externalinterfaces.ShoppingCart;
-import business.externalinterfaces.ShoppingCartSubsystem;
 import business.shoppingcartsubsystem.ShoppingCartSubsystemFacade;
 import alltests.AllTests;
 import junit.framework.TestCase;
@@ -36,7 +34,6 @@ public class DBClassShoppingCartTest extends TestCase {
 			int idRetrieved = Integer.parseInt(retrievedShoppingCart.getCartId());
 			assertTrue(expectedId == idRetrieved);
 		} catch (DatabaseException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
